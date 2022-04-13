83dfcf2bbf29549bc0020a1bd0e35cde02b9e09e
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-13 19:57:08 +0530

    sri push problem

---
+++ MoodAnnalyser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalayzer
+{
+    public class MoodAnalyse
+    {
+        private string message;
+
+        /// <summary>
+        /// Parameterised Constructor.
+        /// </summary>
+        /// <param name="message"></param>
+        public MoodAnalyse(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            if (message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
+
